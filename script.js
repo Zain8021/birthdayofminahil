c1b2b5baef50a19192a954a9d81cfdbabbf5fa28
Zain8021 <muhammadzain8021@gmail.com>
2025-07-27 19:28:51 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById("surpriseBtn").onclick = function () {
+  document.getElementById("surpriseContent").classList.remove("hidden");
+};
+
+document.getElementById("videoBtn").onclick = function () {
+  document.getElementById("videoContainer").classList.toggle("hidden");
+};
